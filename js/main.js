0eefa51dddc8371d4ddeaec6b768b579eb59a7fe
Janne Parkkila <japskua@gmail.com>
2014-11-11 11:42:29 +0200

    Fixed to login to look nicer

--- js/main.js
+++ js/main.js
@@ -4,38 +4,87 @@
 
 $(document).ready(function() {
 
+    // Button functions
     $('#gamecloud-login-button').on('click', Login);
+    $('#gamecloud-logout-button').on('click', Logout);
 
-
-    function Login() {
-        var username = $('#gamecloud-login-username').val();
-        var password = $('#gamecloud-login-password').val();
-        console.log("Username is:", username, "password is:", password);
-
-        var json = {
-            "callType": "loginUser",
-            "username": username,
-            "password": password
-        };
-
-        json = JSON.stringify(json);
-
-        $.post("http://127.0.0.1:8888/api/1/", json, function (data) {
-
-            if (CheckAuth(data)) {
-                // Authentication success
-                alert("Login succesfull!");
-            } else {
-                alert(data);
-            }
-
-        });
-
-    }
-
-    function CheckAuth(authResult) {
-        return (authResult !== "No such player in the system");
-    }
+    // Hide the logout
+    $('#gamecloud-logged').hide();
 
 });
 
+/**
+ * Handles loggin out from the server
+ * @constructor
+ */
+function Logout() {
+    // Show the login
+    $('#gamecloud-login').show();
+    // Empty the fields
+    $('#gamecloud-login-username').val("");
+    $('#gamecloud-login-password').val("");
+    // And hide yourself
+    $('#gamecloud-logged').hide();
+}
+
+/**
+ * Handles logging in to the gamecloud server
+ * @constructor
+ */
+function Login() {
+    var json = ReadCredentials();
+    SendToServer(json);
+}
+
+/**
+ * Reads the user credentials from the webpage
+ * @returns {JSON} Stringified JSON
+ * @constructor
+ */
+function ReadCredentials() {
+    var username = $('#gamecloud-login-username').val();
+    var password = $('#gamecloud-login-password').val();
+    console.log("Username is:", username, "password is:", password);
+
+    var json = {
+        "callType": "loginUser",
+        "username": username,
+        "password": password
+    };
+
+    return JSON.stringify(json);
+}
+
+/**
+ * Sends the stringified json to server
+ * @param {JSON} json the stringified JSON sent to the server
+ * @constructor
+ */
+function SendToServer(json) {
+    $.post("http://127.0.0.1:8888/api/1/", json, function (data) {
+
+        if (CheckAuth(data)) {
+            // Authentication success
+            alert("Login succesfull!");
+            // Hide yourself
+            $('#gamecloud-login').hide();
+            // And show the logged
+            $('#gamecloud-logged').show();
+            // And set username
+            $('#gamecloud-username').text($('#gamecloud-login-username').val());
+        } else {
+            alert(data);
+        }
+
+    });
+}
+
+/**
+ * Checks for auth results
+ * @param {String} authResult The results of the authentication
+ * @returns {boolean} depending on whether auth was succesful or not
+ * @constructor
+ */
+function CheckAuth(authResult) {
+    return (authResult !== "No such player in the system");
+}
