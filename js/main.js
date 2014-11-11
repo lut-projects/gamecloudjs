0cf56394beb5f94c296f372219621ce6445db3dc
Janne Parkkila <japskua@gmail.com>
2014-11-11 11:24:14 +0200

    Login works now properly, thanks to jQuery

--- js/main.js
+++ js/main.js
@@ -4,25 +4,38 @@
 
 $(document).ready(function() {
 
-    $('#gamecloud-login-form').submit(function (e) {
+    $('#gamecloud-login-button').on('click', Login);
 
+
+    function Login() {
         var username = $('#gamecloud-login-username').val();
         var password = $('#gamecloud-login-password').val();
         console.log("Username is:", username, "password is:", password);
 
         var json = {
-            "callType" : "loginUser",
-            "username" : username,
-            "password" : password
+            "callType": "loginUser",
+            "username": username,
+            "password": password
         };
 
         json = JSON.stringify(json);
 
-        $.post("http://127.0.0.1:8888/api/1/", json, function(data) {
+        $.post("http://127.0.0.1:8888/api/1/", json, function (data) {
 
-            alert("result of post is:" + data);
+            if (CheckAuth(data)) {
+                // Authentication success
+                alert("Login succesfull!");
+            } else {
+                alert(data);
+            }
+
         });
 
-    });
+    }
+
+    function CheckAuth(authResult) {
+        return (authResult !== "No such player in the system");
+    }
 
 });
+
